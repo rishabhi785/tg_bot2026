0f8e5da29268f41d5980152c2e6f5a67ccbe6920
rishabhi785 <rishabhsaroj8770@gmail.com>
2026-05-23 21:21:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ PORT = int(os.getenv("PORT", "8000"))
 ADMIN_ID = 7856754202
 
 REPLIT_DOMAINS = os.getenv("REPLIT_DOMAINS", "")
-MANUAL_WEBAPP_URL = "https://frontend-2026-device-verification.vercel.app/"
+MANUAL_WEBAPP_URL = "https://tg-bot2026.vercel.app/"
 
 if MANUAL_WEBAPP_URL:
     WEBAPP_URL = MANUAL_WEBAPP_URL
